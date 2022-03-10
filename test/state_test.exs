f616216e3361104ce467d9a3cdc3c007d8408707
Rafael Antunes <dev@rafaelantun.es>
2022-03-10 19:28:36 -0300

    Add State tests

---
+++ test/state_test.exs
@@ -0,0 +1,46 @@
+defmodule ExNews.StateTest do
+  use ExUnit.Case, async: true
+
+  alias ExNews.Test.{StateUtils}
+  alias ExNews.{State}
+
+  setup do
+    on_exit(fn ->
+      StateUtils.wipe_state()
+    end)
+
+    :ok
+  end
+
+  describe "write/1" do
+    test "it stores the entries in the ETS table" do
+      assert [] == StateUtils.get_all_items()
+
+      entries = [
+        %{"id" => 1},
+        %{"id" => 2}
+      ]
+
+      State.write(entries)
+
+      assert [items: result] = StateUtils.get_all_items()
+      assert Enum.sort(result) == Enum.sort(entries)
+    end
+  end
+
+  describe "single_lookup/1" do
+    test "returns the entry when it exists" do
+      assert [] == StateUtils.get_all_items()
+
+      State.write([%{"id" => 1}])
+      assert %{"id" => 1} == State.single_lookup(1)
+    end
+
+    test "returns nil when entry does not exist" do
+      assert [] == StateUtils.get_all_items()
+
+      State.write([%{"id" => 1}])
+      assert nil == State.single_lookup(2)
+    end
+  end
+end
